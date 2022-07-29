9dd2dce3ad6d0d790c1a56c101d1dfaa01ae2138
Ayazelar <92436793+Ayazelar@users.noreply.github.com>
2022-07-29 10:44:35 +0300

    feat: added messages after signup request,changed company fields (#4)

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,30 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { CompanyApplication } from 'src/app/interfaces/company';
 import { CompanyService } from 'src/app/services/company.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
+  providers: [MessageService]
+
 })
 export class SignUpComponent implements OnInit {
 
-  companyApplication: CompanyApplication = {
+  companyApplication: any = {
     company: {
       name: '',
-      short_name: '',
+      zip: '',
+      city: '',
+      address: '',
+      country: ''
     },
     admin: {
       name: '',
       surname: '',
       phone_number: '',
       email: '',
-      password:'',
+      password: '',
     }
   }
+  loading: boolean = false;
 
-  constructor(private _router: Router, private _companyService: CompanyService) { }
+  constructor(private _router: Router, private _companyService: CompanyService, private _messageService: MessageService) { }
 
   ngOnInit(): void {
   }
@@ -33,7 +40,19 @@ export class SignUpComponent implements OnInit {
     this._router.navigate(['login'])
   }
 
-  signUp() {
-    this._companyService.signUp(this.companyApplication)
+  async signUp() {
+    this.loading = true
+    await this._companyService.signUp(this.companyApplication).then((r: any) => {
+      this._messageService.add({ severity: 'info', summary: r.message })
+
+      for (let field in this.companyApplication.company) this.companyApplication.company[field] = '';
+      for (let field in this.companyApplication.admin) this.companyApplication.admin[field] = '';
+
+    }).catch((e) => {
+      this._messageService.add({ severity: 'error', summary: e.error.message.message });
+    }).finally(() => {
+      this.loading = false
+    })
+
   }
 }
